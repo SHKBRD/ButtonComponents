22578e74e385317867e7e358ca94a763d5db4e74
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2026-03-24 16:39:54 -0400

    add others

--- scenes/controlcomponents/TransformOnHover/transform_on_hover.gd
+++ scenes/controlcomponents/TransformOnHover/transform_on_hover.gd
@@ -12,19 +12,49 @@ class_name TransformOnHover_CC
 @export var endScale: Vector2 = Vector2i(1.0, 1.0)
 
 @export_group("Tweening")
-@export var easing: Tween.EaseType
-@export var transition: Tween.TransitionType
-@export var time: float
+@export var tweenEasing: Tween.EaseType
+@export var tweenTransition: Tween.TransitionType
+@export var tweenTime: float
 
-@export_group("Trigger Functions")
-@export var growTriggerFuncName: Array[String]
-@export var shrinkTriggerFuncName: Array[String]
+var selected: bool = false
+var currentTween: Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func attempt_enable() -> void:
+	if selected:
+		return
+	selected = false
+	currentTween = create_tween()
+	currentTween.set_ease(tweenEasing).set_trans(tweenTransition)
+	currentTween.parallel().tween_property(self, "position", endOffsetPosition, tweenTime)
+	currentTween.parallel().tween_property(self, "rotation", endOffsetRotation, tweenTime)
+	currentTween.parallel().tween_property(self, "scale", endScale, tweenTime)
+
+func attempt_disable() -> void:
+	if not selected:
+		return
+	selected = false
+	currentTween = create_tween()
+	currentTween.set_ease(tweenEasing).set_trans(tweenTransition)
+	currentTween.parallel().tween_property(self, "position", startOffsetPosition, tweenTime)
+	currentTween.parallel().tween_property(self, "rotation", startOffsetRotation, tweenTime)
+	currentTween.parallel().tween_property(self, "scale", startScale, tweenTime)
+
+func on_focus_entered() -> void:
+	attempt_enable()
+
+func on_mouse_entered() -> void:
+	attempt_enable()
+	
+func on_focus_exited() -> void:
+	attempt_disable()
+	
+func on_mouse_exited() -> void:
+	attempt_disable()
